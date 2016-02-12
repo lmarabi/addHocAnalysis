c1b7fa0f88095c96ab7a7f74c5a5feeb33d0f574
louai <louai@x-10-104-104-198.uofm-secure.wireless.umn.edu>
2016-02-12 10:27:15 -0600

    working on keywordSearch

--- src/org/umn/keyword/keywordSearch.java
+++ src/org/umn/keyword/keywordSearch.java
@@ -8,45 +8,61 @@ import java.io.InputStreamReader;
 import org.umn.index.RectangleQ;
 
 public class keywordSearch {
-	int[] count; 
-	RectangleQ[] mbrs;
-	
-	public keywordSearch(int size) {
-		 this.count = new int[size] ;
-		 this.mbrs = new RectangleQ[size]; 
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		RectangleQ[] mbr = {new RectangleQ(-180,-90,180,90)};
-		keywordSearch temp = keywordSearch.match(mbr);
-		
+		RectangleQ[] mbr = { new RectangleQ(-180, -90, 180, 90) };
+		keywordSearch.match(mbr, "king");
+
 	}
-	
+
 	/**
-	 * This method takes an array of mbrs and find keywords, that match a given keyword
+	 * This method takes an array of mbrs and find keywords, that match a given
+	 * keyword
+	 * 
 	 * @param mbr
 	 * @return
 	 * @throws IOException
 	 */
-	public static keywordSearch match(RectangleQ[] mbr) throws IOException{
-		keywordSearch result = new keywordSearch(mbr.length);
-		String fileName = System.getProperty("user.dir")+"/dataset/part-r-00000";
-		BufferedReader br = new BufferedReader(new InputStreamReader((new FileInputStream(fileName))));
+	public static int[] match(RectangleQ[] mbr, String keyword)
+			throws IOException {
+		int[] count = new int[mbr.length];
+		String fileName = System.getProperty("user.dir")
+				+ "/dataset/part-r-00000";
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(new FileInputStream(fileName))));
 		String line;
+		RectangleQ test = null;
 
 		long start = System.currentTimeMillis();
 		while ((line = br.readLine()) != null) {
-			String[] list = line.split("\t");
-			for(int i=0 ; i< mbr.length ; i++){
-				//System.out.println(list[0]);
+
+			for (int i = 0; i < mbr.length; i++) {
+				String[] xy = line.substring(0, line.indexOf("\t")).split(",");
+				test = new RectangleQ(Double.parseDouble(xy[0]),
+						Double.parseDouble(xy[1]), Double.parseDouble(xy[2]),
+						Double.parseDouble(xy[3]));
+				if (mbr[i].isIntersected(test)) {
+					String[] list = line.split("\t");
+					for (int j = 2; j < list.length; j++) {
+						if (list[j].contains(keyword)) {
+							String[] keyvalue = list[j].split(",");
+							if (keyvalue[0].equals(keyword)) {
+								//System.out.println(list[j]);
+								count[i] = Integer.parseInt(keyvalue[1]);
+								break;
+							}
+						}
+					}
+				}
+
 			}
-			
+
 		}
 		long end = System.currentTimeMillis();
 		br.close();
-		System.out.println("program end Time: "+(end -start));
-		return result;
+		System.out.println("program end Time: " + (end - start));
+		return count;
 	}
 
 }
